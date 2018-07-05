f160210bbb2e411a4dc0205d02122f6f21190015
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-05 18:37:58 -0300

    Implementada suma

---
+++ src/main/java/com/udemy/secondassignment/SecondAssignment/AppImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.udemy.secondassignment.SecondAssignment;
+
+/**
+ * @author lunavi97
+ *
+ */
+public class AppImpl implements App {
+
+	/* (non-Javadoc)
+	 * @see com.udemy.secondassignment.SecondAssignment.App#sum(int, int)
+	 */
+	@Override
+	public int sum(int a, int b) throws Exception {
+		return a + a;
+	}
+
+}
