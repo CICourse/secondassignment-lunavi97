b3f32a9cd33ef544361301c316cb32948a9364b0
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-07-23 23:26:19 -0300

    Agregada documentación del método sum

--- src/main/java/com/udemy/secondassignment/SecondAssignment/App.java
+++ src/main/java/com/udemy/secondassignment/SecondAssignment/App.java
@@ -7,5 +7,11 @@ package com.udemy.secondassignment.SecondAssignment;
  */
 public interface App 
 {
+    /**
+     * @param a
+     * @param b
+     * @return la suma de a y b
+     * @throws Exception
+     */
     public abstract int sum(final int a, final int b) throws Exception;
 }
